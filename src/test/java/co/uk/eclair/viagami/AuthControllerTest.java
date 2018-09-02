cde7194ae88059be96084ef2664702a948496500
Eclair <eclairlumu@gmail.com>
2018-09-02 19:37:49 +0100

    test class created for authcontroller

---
+++ src/test/java/co/uk/eclair/viagami/AuthControllerTest.java
@@ -0,0 +1,12 @@
+package co.uk.eclair.viagami;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Created by ${Eclair} on 9/1/2018.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+    
+}
